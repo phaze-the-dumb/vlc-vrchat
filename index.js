d10f05872456542ccc52d287ffca160d6ee66e70
Phaze <57566773+phaze-the-dumb@users.noreply.github.com>
2022-12-27 17:28:47 +0000

    update a bunch of shit to fix issues

--- index.js
+++ index.js
@@ -2,17 +2,51 @@ const { spawn } = require('child_process');
 const { Client } = require('node-osc');
 const fetch = require('node-fetch');
 
-spawn('vlc', [ '--http-password', 'f' ]);
+spawn('C:/Program Files/VideoLAN/VLC/vlc.exe', [ '--http-password', 'f' ]);
 const client = new Client('127.0.0.1', 9000);
+
+let text = '';
+let prevText = '';
 
 setInterval(() => {
     fetch('http://:f@127.0.0.1:8080/requests/status.json').then(data => data.json()).then(data => {
-        if(data.state === 'playing'){
-            client.send('/chatbox/input', [ '♫ ' + data.information.category.meta.title + ' - ' + data.information.category.meta.artist, true ], () => {});
+        if(!data.information){
+            text = '';
+        } else if(data.state === 'playing'){
+            if(data.information.category.meta.title){
+                text = '♫ ' + data.information.category.meta.title + ' - ' + data.information.category.meta.artist;
+            } else if(data.information.category.meta.filename){
+                let name = data.information.category.meta.filename.split('.');
+                name.pop();
+                name = name.join('.');
+
+                text = '♫ ' + name;
+            } else{
+                text = '';
+            }
         } else if(data.state === 'paused'){
-            client.send('/chatbox/input', [ '♫ ' + data.information.category.meta.title + ' - ' + data.information.category.meta.artist, true ], () => {});
+            if(data.information.category.meta.title){
+                text = '❚❚ ' + data.information.category.meta.title + ' - ' + data.information.category.meta.artist;
+            } else if(data.information.category.meta.filename){
+                let name = data.information.category.meta.filename.split('.');
+                name.pop();
+                name = name.join('.');
+
+                text = '❚❚ ' + name;
+            } else{
+                text = '';
+            }
         } else{
+            text = '';
+        }
+
+        if(text !== prevText){
+            prevText = text;
             client.send('/chatbox/input', [ '', true ], () => {});
+            console.log(text);
+
+            if(!data.information.category.meta.title)
+                console.log('VLC Cannot find files metadata, we will just display the filename.')
         }
     })
-}, 10000);
+}, 1000);
